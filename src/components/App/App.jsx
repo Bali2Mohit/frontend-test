4dc192fd108d1a1133512d975728f5e718cdcf42
Mohit Bali <bali2mohit@gmail.com>
2023-11-17 20:17:03 +0000

    Integerated PropertyListing with API

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import Header from '../Header';
 import SortAndFilter from '../SortAndFilter';
 import PropertyListing from '../PropertyListing';
 
 const App = () => {
+    const [propertyList, setPropertyList] = useState();
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/properties')
+            .then((res) => res.json())
+            .then((data) => setPropertyList(data))
+            .catch((error) => alert(error));
+    }, []);
+
     return (
         <div className="App">
             <Header />
             <main>
                 <SortAndFilter />
-                <PropertyListing />
+                <PropertyListing propertyList={propertyList} />
             </main>
         </div>
     );
--- src/components/PropertyListing/tests/PropertyListing.spec.jsx
+++ src/components/PropertyListing/tests/PropertyListing.spec.jsx
@@ -3,11 +3,28 @@ import { render, screen } from '@testing-library/react';
 import { within } from '@testing-library/dom';
 import PropertyListing from '../PropertyListing';
 
+const mockPropertyList = [
+    { propertyTitle: 'Some title' },
+    { propertyTitle: 'Some title1' },
+    { propertyTitle: 'Some title2' },
+    { propertyTitle: 'Some title3' },
+    { propertyTitle: 'Some title4' },
+    { propertyTitle: 'Some title5' },
+    { propertyTitle: 'Some title6' },
+    { propertyTitle: 'Some title7' },
+];
+
 describe('PropertyListing', () => {
     it('should render five property cards', async () => {
-        render(<PropertyListing />);
+        render(<PropertyListing propertyList={mockPropertyList} />);
         const propertiesList = screen.getByRole('list');
         const propertyCards = await within(propertiesList).findAllByRole('listitem');
         expect(propertyCards).toHaveLength(5);
+    });
+
+    it('should return empty list for undefined data', async () => {
+        render(<PropertyListing propertyList={undefined} />);
+        const propertiesList = screen.getByRole('list');
+        expect(propertiesList).toBeEmptyDOMElement();
     });
 });
